83aac72ffe48cbf2f724ffaefaee05b8b3d1d9b0
wang <wang4012055@gmail.com>
2019-12-02 16:46:47 +0800

    上传禁用

--- src/js/upload-songs.js
+++ src/js/upload-songs.js
@@ -1,21 +1,18 @@
+ window.flag ="open"
  //配置
  $.cloudinary.config({
     cloud_name: 'dah1jcepx',
     secure: true
 });
 
-
-
 //fileupload绑定
 $(document).ready(function () {
     if ($.fn.cloudinary_fileupload !== undefined) {
         $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload();
-        console.log("ok");
     }
 });
 //上传事件绑定
 $('.cloudinary-fileupload').bind('cloudinarydone', function (e, data) {
-
     console.log(data)
     let img = $.cloudinary.url(data.result.public_id)
     if(data.result.original_filename ===undefined){
@@ -31,6 +28,14 @@ $('.cloudinary-fileupload').bind('cloudinarydone', function (e, data) {
         // $('.image_public_id').val(data.result.public_id)
     return true;
 });
+$('.cloudinary-fileupload').bind('fileuploadstart',(e,data)=>{
+    console.log("禁用")
+    $('.cloudinary-fileupload').fileupload('disable');
+})
+$('.cloudinary-fileupload').bind('fileuploadstop',(e,data)=>{
+    console.log("启用")
+    $('.cloudinary-fileupload').fileupload('enable');
+})
 //进度条
 $('.cloudinary-fileupload').bind('cloudinaryprogress', function (e, data) {
     $('.progress_bar').css('width', Math.round((data.loaded * 100.0) / data.total) + '%');
